ff8a9b793a229ee6f056caab94ce9194e2c11a12
ksa237 <ksa237@yandex.ru>
2025-04-20 01:28:16 +1000

    hw1_1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //++ exercise1
         System.out.println("Sergey Kazakov");
+        //-- exercise1
+
+        //++exercise2
+        int income;
+        int spending;
+
+        income=44000;
+        spending=30000;
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+        //-- exercise2
     }
 }
